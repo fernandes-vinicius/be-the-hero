8858e940bd4f69bbfdf9b90ea4e5af5f100d778b
VinyFernandes <vinicius_sfo@hotmail.com>
2020-03-27 14:53:09 -0300

    Add testes backend

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,8 +1,8 @@
-const crypto = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId');
 const connection = require('../database/connection');
 
 module.exports = {
-    async index(require, response) {
+    async index(request, response) {
         const ongs = await connection('ongs').select('*');
 
         return response.json(ongs);
@@ -11,7 +11,7 @@ module.exports = {
     async create(request, response) {
         const { name, email, whatsapp, city, uf } = request.body;
 
-        const id = crypto.randomBytes(4).toString("HEX");
+        const id = generateUniqueId();
 
         await connection('ongs').insert({
             id,
